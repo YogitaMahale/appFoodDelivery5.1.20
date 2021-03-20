5b912ad098c8c35fadc984a0b9bbe4610369131f
yogita mahale <mahaleyogita233@gmail.com>
2021-03-19 16:35:10 -1200

    New Sms Sent Integration

--- appFoodDelivery.Utility/SD.cs
+++ appFoodDelivery.Utility/SD.cs
@@ -8,11 +8,14 @@ namespace appFoodDelivery.Utility
 {
     public static class SD
     {
-        
-        public const string SMS_OPTINS = "MSGSAY";
-        public const string SMS_Password = "959595";
-        public const string SMS_user = "Aveebroiler";
-       
+
+        //public const string SMS_OPTINS = "MSGSAY";
+        //public const string SMS_Password = "959595";
+        //public const string SMS_user = "Aveebroiler";
+
+        public const string SMS_OPTINS = "TOFOZF";
+        public const string SMS_Password = "78746582";
+        public const string SMS_user = "t1pichome";
 
     }
 }
--- appFoodDelivery.Utility/SendSMS.cs
+++ appFoodDelivery.Utility/SendSMS.cs
@@ -14,14 +14,9 @@ namespace appFoodDelivery.Utility
             try
             {
 
-                //string Password = "Bingo@5151";
-              
-                //string userdetails = "ezacus";
-                //string OPTINS = "MSGSAY";
-                //string MobileNumber = mobileNo;
-                // string type = "3";
-                string template_id = "123";
-                string strUrl = "https://bulksms.co/sendmessage.php?&user=" + SD.SMS_user + "&password=" + SD.SMS_Password + "&mobile=" + mobileNo + "&message=" + message  + "&sender=" + SD.SMS_OPTINS + "&type=" + 3 + "&template_id=" + template_id;
+
+              //  string template_id = "123";
+                string strUrl = "https://nimbusit.co.in/api/swsend.asp?username="+SD.SMS_user+"&password="+SD.SMS_Password+"&sender="+SD.SMS_OPTINS+"&sendto="+mobileNo+"&message="+message;
 
                 ServicePointManager.Expect100Continue = true;
                 ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12;
@@ -41,7 +36,37 @@ namespace appFoodDelivery.Utility
                 return false;
             }
             #endregion
-             
+
         }
+        //public bool smsSent(string mobileNo, string message)
+        //{
+        //    #region "sms"
+        //    try
+        //    {
+
+
+        //        string template_id = "123";
+        //        string strUrl = "https://bulksms.co/sendmessage.php?&user=" + SD.SMS_user + "&password=" + SD.SMS_Password + "&mobile=" + mobileNo + "&message=" + message  + "&sender=" + SD.SMS_OPTINS + "&type=" + 3 + "&template_id=" + template_id;
+
+        //        ServicePointManager.Expect100Continue = true;
+        //        ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12;
+        //        System.Net.WebRequest request = System.Net.WebRequest.Create(strUrl);
+        //        HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+        //        Stream s = (Stream)response.GetResponseStream();
+        //        StreamReader readStream = new StreamReader(s);
+        //        string dataString = readStream.ReadToEnd();
+        //        s.Close();
+        //        readStream.Close();
+        //        response.Close();
+        //        return true;
+        //    }
+
+        //    catch
+        //    {
+        //        return false;
+        //    }
+        //    #endregion
+
+        //}
     }
 }
